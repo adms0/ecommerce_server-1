cbb5d6368b0763fcc6147f9832fc01867bc642ac
ams0 <adly.ms@gmail.com>
2020-11-14 17:44:40 +0700

    change port to env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = require("./routes/index")
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 
 const errorHandler = require("./middlewares/errorhandler.middleware")
 
